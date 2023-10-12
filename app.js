865d8c57763e1babaa828d5759be0c8acf3e09b1
Divyapalsinh Vala <divyapalsinh@aramex.com>
2023-10-12 17:42:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => res.type('html').send(html));
 app.post("/asd", (req, res) => {
-  console.log(req, 'dvvla')
+  console.log(req.body, 'dvvla')
   return res.type('html').send(html)
 });
 
